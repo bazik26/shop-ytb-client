2ab7ad9691f77cfc808a0b2af961877ba2e6b350
bazik26 <bazikbazik26@gmail.com>
2025-02-25 14:58:02 +0300

    add serch

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,6 +47,7 @@ function Dashboard() {
           content="Ваш надёжный партнёр в мире автомобилей.
         Предлагаем широкий выбор европейских авто по доступным ценам. Узнайте больше на нашем сайте."
         />
+        <meta name="yandex-verification" content="16d84bd4db80e8d0" />
         {/* <script src="//code.jivosite.com/widget/gNbKuWhZqe" async></script> */}
       </Head>
       {shouldLoadContent && (
