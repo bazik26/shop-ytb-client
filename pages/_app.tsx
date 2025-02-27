b1c575b56220d9813002836db04fb2c3d0f4decc
bazik26 <bazikbazik26@gmail.com>
2025-02-27 14:32:59 +0300

    vkpicsel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { withHydrate } from 'effector-next'
 import { useEffect, useState } from 'react'
 import { ToastContainer } from 'react-toastify'
 import NextNProgress from 'nextjs-progressbar'
+import Script from 'next/script'
 import 'react-toastify/dist/ReactToastify.css'
 import '@/styles/globals.css'
 
@@ -28,6 +29,32 @@ function App({ Component, pageProps }: AppProps) {
   return (
     mounted && (
       <>
+        {/* Подключаем пиксель Top.Mail.Ru */}
+        <Script id="top-mail-ru" strategy="lazyOnload">
+          {`
+            var _tmr = window._tmr || (window._tmr = []);
+            _tmr.push({id: "3618651", type: "pageView", start: (new Date()).getTime()});
+            (function (d, w, id) {
+              if (d.getElementById(id)) return;
+              var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+              ts.src = "https://top-fwz1.mail.ru/js/code.js";
+              var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+              if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+            })(document, window, "tmr-code");
+          `}
+        </Script>
+
+        {/* Fallback для пользователей без JS */}
+        <noscript>
+          <div>
+            <img
+              src="https://top-fwz1.mail.ru/counter?id=3618651;js=na"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt="Top.Mail.Ru"
+            />
+          </div>
+        </noscript>
+
         <NextNProgress />
         <Component {...pageProps} />
         <ToastContainer
