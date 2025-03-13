863ab04718d13f0440501f5816e0f221730da952
bazik26 <bazikbazik26@gmail.com>
2025-03-13 13:17:52 +0300

    faviconfix2

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,6 +49,12 @@ function Dashboard() {
 
         {/* Favicon */}
         <link rel="icon" type="image/png" sizes="32x32" href="/img/logo.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Cars Euro" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         {/* SEO МЕТАТЕГИ */}
         <meta
